bbc3fd68018fdb9b2588a3446a98eb37a10f6e1e
Arnaud Beaulieu <arnaudbeaulieu@MacBook-Pro-de-Arnaud.local>
2024-06-27 15:18:37 +0200

    WIP: integrate mobile version

--- components/home/section5.tsx
+++ components/home/section5.tsx
@@ -18,7 +18,7 @@ export default function Section5(){
                 <div className="flex gap-1">
                     <div className="bg-secondary-300 text-secondary-200 w-[49%] rounded-2xl p-4 flex flex-col">
                         <p className="font-bold">Les prépartion secrète :</p>
-                        <p>Comment organiser une surprise romantique avec une location à l’heure ?</p>
+                        <p>Comment organiser une surprise romantique avec une location à l&apos;heure ?</p>
                         <div className="w-full flex justify-end ">
                             <svg className="w-6 h-6 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#FFFFFF" d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
                         </div>
--- components/home/section6.tsx
+++ components/home/section6.tsx
@@ -7,7 +7,7 @@ export default function Section6(){
                 <div className="w-full h-full bg-primary rounded-2xl text-secondary-200 p-8 flex flex-col gap-4">
                     <h2 className="text-center text-[18px]">
                         Inscrivez-vous à notre newsletter!<br></br>
-                        Promis, c'est plus excitant qu'un<br></br>
+                        Promis, c&apos;est plus excitant qu&apos;un<br></br>
                         épisode de Peppa Pig.
                     </h2>
                     <div className="text-[12px] w-full bg-secondary-200 flex justify-between py-2 rounded-full px-2">
