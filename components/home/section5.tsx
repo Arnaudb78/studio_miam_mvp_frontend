f52353aea5066a2b1afe77dc1cb93952246b9b58
Arnaud Beaulieu <arnaudbeaulieu@MacBook-Pro-de-Arnaud.local>
2024-06-27 15:19:40 +0200

    WIP: integrate mobile version

--- components/home/section5.tsx
+++ components/home/section5.tsx
@@ -6,7 +6,7 @@ export default function Section5(){
           <section className="w-full h-full px-2 text-[13px]">
             <div className="flex flex-col gap-1">
                 <div className="flex flex-col gap-2 justify-start  border border-solid border-gray-300 p-4 rounded-2xl">
-                    <p className="font-bold">Des conseils amoureux savoureux qu'une<br></br> nuit complète de sommeil :</p>
+                    <p className="font-bold">Des conseils amoureux savoureux qu&apos;une<br></br> nuit complète de sommeil :</p>
                     <p>Lorem ipsum dolor sit amet, consectetur<br></br> adipiscing elit, sed do eiusmod tempor incididunt<br></br> ut labore et dolore.</p>
                     <div className="w-full flex justify-end ">
                         <svg className="w-6 h-6 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
