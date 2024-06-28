87087631eaa9fc98b828924da0a85b3e4f9c6786
Arnaud Beaulieu <arnaudbeaulieu@MacBook-Pro-de-Arnaud.local>
2024-06-28 14:58:49 +0200

    DONE: add new version for mobile

--- components/appart/card-appart-details.tsx
+++ components/appart/card-appart-details.tsx
@@ -72,14 +72,14 @@ export default function CardAppartDetails({ id }: DetailsClientProps) {
     const searchParams = useSearchParams();
     const type = searchParams.get("type");
     const [data, setData] = useState<AppartsProps | null>(null);
-    const[users, setUsers] = useState<UsersProps | null>(null);
+    const [users, setUsers] = useState<UsersProps | null>(null);
 
     useEffect(() => {
         const fetchAppartDetails = async () => {
             try {
                 const response = await fetch(`http://localhost:5001/apparts/${id}`);
                 if (!response.ok) {
-                    throw new Error('Erreur lors de la récupération des détails de l\'appartement');
+                    throw new Error("Erreur lors de la récupération des détails de l'appartement");
                 }
                 const data = await response.json();
                 setData(data);
@@ -87,20 +87,20 @@ export default function CardAppartDetails({ id }: DetailsClientProps) {
                     await fetchUserInfo(data.user_id);
                 }
             } catch (error) {
-                console.error('Erreur:', error);
+                console.error("Erreur:", error);
             }
         };
 
         const fetchUserInfo = async (userId: string) => {
             try {
-                const response = await fetch(`http://localhost:5001/users/${userId}`);
+                const response = await fetch(`https://pacific-reaches-55510-1cc818501846.herokuapp.com/users/${userId}`);
                 if (!response.ok) {
-                    throw new Error('Erreur lors de la récupération des informations de l\'utilisateur');
+                    throw new Error("Erreur lors de la récupération des informations de l'utilisateur");
                 }
                 const userData = await response.json();
                 setUsers(userData);
             } catch (error) {
-                console.error('Erreur:', error);
+                console.error("Erreur:", error);
             }
         };
 
@@ -115,34 +115,36 @@ export default function CardAppartDetails({ id }: DetailsClientProps) {
         {
             pic: "/images/cha.png",
             name: "Charlotte",
-            description: "Nous avons réservé plusieurs fois sur cette application et nous avons toujours eu une super expériences. Les chambres sont propres et le système de boîte à clé.",
-            date: "10h20 - 14 / 06 / 2024"
+            description:
+                "Nous avons réservé plusieurs fois sur cette application et nous avons toujours eu une super expériences. Les chambres sont propres et le système de boîte à clé.",
+            date: "10h20 - 14 / 06 / 2024",
         },
         {
             pic: "/images/enzo.png",
             name: "Enzo",
-            description: "Nous avons réservé plusieurs fois sur cette application et nous avons toujours eu une super expériences. Les chambres sont propres et le système de boîte à clé.",
-            date: "12h30 - 15 / 06 / 2024"
+            description:
+                "Nous avons réservé plusieurs fois sur cette application et nous avons toujours eu une super expériences. Les chambres sont propres et le système de boîte à clé.",
+            date: "12h30 - 15 / 06 / 2024",
         },
         {
             pic: "/images/louison.png",
             name: "Louison",
-            description: "Nous avons réservé plusieurs fois sur cette application et nous avons toujours eu une super expériences. Les chambres sont propres et le système de boîte à clé.",
-            date: "18h20 - 16 / 06 / 2024"
+            description:
+                "Nous avons réservé plusieurs fois sur cette application et nous avons toujours eu une super expériences. Les chambres sont propres et le système de boîte à clé.",
+            date: "18h20 - 16 / 06 / 2024",
         },
     ];
 
     const comments = commentary.map((comment, index) => {
-        return <CardComment2 key={index} pic={comment.pic} name={comment.name} description={comment.description} date={comment.date} />
-    })
-
+        return <CardComment2 key={index} pic={comment.pic} name={comment.name} description={comment.description} date={comment.date} />;
+    });
 
     if (!data) {
         return (
             <>
                 <Navbar />
                 <div>Loading...</div>
-                <Footer/>
+                <Footer />
             </>
         );
     }
@@ -154,8 +156,8 @@ export default function CardAppartDetails({ id }: DetailsClientProps) {
                 <img className="rounded-2xl" src={data.images[0]} alt="image de l'appartement" />
                 <div className="flex flex-col gap-2 mt-10">
                     <div className="border border-solid border-primary p-4 flex flex-col gap-4 rounded-2xl">
-                    <p className="font-bold text-[16px]">/ Description du logement</p> 
-                    <p>{data.description}</p>
+                        <p className="font-bold text-[16px]">/ Description du logement</p>
+                        <p>{data.description}</p>
                     </div>
                     <div className="bg-[#F4F3EB] p-4 rounded-2xl flex flex-col gap-4">
                         <p className="text-[16px] font-bold">/ Votre hôte</p>
@@ -168,26 +170,40 @@ export default function CardAppartDetails({ id }: DetailsClientProps) {
                         </div>
                         <div>{data.description}</div>
                         <div className="flex flex-col gap-2">
-                            <p><span className="font-bold">Langues : </span>Français, Anglais, Espagnols</p>
-                            <p><span className="font-bold">Temps de réponse : </span>15 min en moyenne</p>
+                            <p>
+                                <span className="font-bold">Langues : </span>Français, Anglais, Espagnols
+                            </p>
+                            <p>
+                                <span className="font-bold">Temps de réponse : </span>15 min en moyenne
+                            </p>
                         </div>
                     </div>
                 </div>
                 <div className="flex flex-col bg-primary text-secondary-200 rounded-2xl p-4 gap-4 mt-10">
-                    <h2 className="text-[22px]">La map pour retrouver le <span className="bg-secondary-300 text-secondary-200 rotate-3 inline-block">chemin</span> de vos désirs !</h2>
-                    <p className="opacity-75">Ce charmant appartement fonctionnel disposant d'un balcon donnant
-                         côté cour est parfait pour découvrir Paris. Dans un quartier animé
-                          (nombreux bars proches du canal) et avec de nombreux commerces, l'appartement
-                           est pourtant parfaitement au calme.</p>
-                    <p className="text-[16px]">{data.localisation.address} {data.localisation.zip_code} {data.localisation.city}</p>
+                    <h2 className="text-[22px]">
+                        La map pour retrouver le <span className="bg-secondary-300 text-secondary-200 rotate-3 inline-block">chemin</span> de vos
+                        désirs !
+                    </h2>
+                    <p className="opacity-75">
+                        Ce charmant appartement fonctionnel disposant d'un balcon donnant côté cour est parfait pour découvrir Paris. Dans un quartier
+                        animé (nombreux bars proches du canal) et avec de nombreux commerces, l'appartement est pourtant parfaitement au calme.
+                    </p>
+                    <p className="text-[16px]">
+                        {data.localisation.address} {data.localisation.zip_code} {data.localisation.city}
+                    </p>
                     <img src="/images/map.png" alt="photo de la map du quartier" />
                 </div>
                 <div className="w-full h-full bg-[#F4F3EB] p-4 flex flex-col gap-1 rounded-2xl">
-                    <p className="text-[20px] my-10">Du chaos familial au <span className="bg-secondary-100 text-secondary-200 rotate-3 inline-block">paradis</span> romantique, découvrez ce qu'ils en disent !</p>
+                    <p className="text-[20px] my-10">
+                        Du chaos familial au <span className="bg-secondary-100 text-secondary-200 rotate-3 inline-block">paradis</span> romantique,
+                        découvrez ce qu'ils en disent !
+                    </p>
                     {comments}
                 </div>
                 <div className="bg-transparent backdrop-blur-lg fixed flex justify-center items-center p-8 w-full bottom-0 left-0">
-                    <button onClick={handleClick} className="bg-secondary-300 p-4 text-secondary-200 rounded-full">Pré-réserver en vitfesse</button>
+                    <button onClick={handleClick} className="bg-secondary-300 p-4 text-secondary-200 rounded-full">
+                        Pré-réserver en vitfesse
+                    </button>
                 </div>
                 {/* <h1>{data.title}</h1>
                 <p>{data.description}</p>
@@ -283,7 +299,7 @@ export default function CardAppartDetails({ id }: DetailsClientProps) {
                     Je réserve en vitfesse
                 </button> */}
             </div>
-            <Footer/>
+            <Footer />
         </div>
     );
 }
--- components/home/section2.tsx
+++ components/home/section2.tsx
@@ -51,13 +51,13 @@ interface AppartsProps {
     images: string[];
 }
 
-export default function Section2(){
+export default function Section2() {
     const router = useRouter();
     const [isProfil, setIsProfil] = useState(false);
     const [apparts, setapparts] = useState<AppartsProps[]>([]);
 
     const getData = async () => {
-        const response = await fetch("http://localhost:5001/apparts");
+        const response = await fetch("https://pacific-reaches-55510-1cc818501846.herokuapp.com/apparts");
         const data = await response.json();
         setapparts(data);
     };
@@ -65,25 +65,34 @@ export default function Section2(){
     useEffect(() => {
         getData();
     }, []);
-    
+
     return (
         <>
-           <section className="p-2 font-satoshi flex flex-col gap-10">
+            <section className="p-2 font-satoshi flex flex-col gap-10">
                 <div className="w-full flex justify-center text-center text-[12px] font-bold gap-1">
                     <div className="w-full flex flex-col items-center bg-[#F4F3EB] p-4 rounded-2xl gap-8">
                         <img src="images/pink_square.png" alt="logo" className="w-10 h-10" />
-                        <p>Annulation gratuite<br />jusqu&apos;à la dernière minute</p>
+                        <p>
+                            Annulation gratuite
+                            <br />
+                            jusqu&apos;à la dernière minute
+                        </p>
                     </div>
                     <div className="w-full flex flex-col items-center bg-[#F4F3EB] p-4 rounded-2xl gap-8">
                         <img src="images/green_square.png" alt="logo" className="w-10 h-10" />
-                        <p>Paiement en ligne<br />pour une discrétion totale</p>
+                        <p>
+                            Paiement en ligne
+                            <br />
+                            pour une discrétion totale
+                        </p>
                     </div>
                 </div>
                 <div className="flex flex-col gap-8">
                     <div className="text-center">
-                        <h2 className="text-[24px]">Nos <span className="bg-secondary-100 text-secondary-200 rotate-3 inline-block">chambres</span> vous attendent !</h2>
-                        <p className="text-[16px] px-8">Faites une pause des réunions Zoom 
-                        et des cris d&apos;enfants ?</p>
+                        <h2 className="text-[24px]">
+                            Nos <span className="bg-secondary-100 text-secondary-200 rotate-3 inline-block">chambres</span> vous attendent !
+                        </h2>
+                        <p className="text-[16px] px-8">Faites une pause des réunions Zoom et des cris d&apos;enfants ?</p>
                     </div>
                     <div className="flex overflow-x-auto whitespace-nowrap w-full">
                         <div className="w-full h-w-full mr-1 flex-zero-zero-auto">
@@ -91,13 +100,14 @@ export default function Section2(){
                                 <CardAppart key={index} {...apparts} isProfil={isProfil} />
                             ))}
                         </div>
-    
                     </div>
                     <div className="w-full flex justify-center text-secondary-200 text-[14px]">
-                        <a className="bg-secondary-300 p-2 rounded-full cursor-pointer" href="/room">En savoir plus</a>
+                        <a className="bg-secondary-300 p-2 rounded-full cursor-pointer" href="/room">
+                            En savoir plus
+                        </a>
                     </div>
                 </div>
-           </section>
+            </section>
         </>
     );
 }
