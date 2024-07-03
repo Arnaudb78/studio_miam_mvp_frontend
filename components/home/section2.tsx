ce8629d3aac8c8651c99dae66a7420a61649465e
Arnaudb78 <arnaudb92230@gmail.com>
2024-07-03 22:00:30 +0200

    CLEAN: adjustement mobile version

--- components/home/section2.tsx
+++ components/home/section2.tsx
@@ -57,7 +57,7 @@ export default function Section2() {
     const [apparts, setapparts] = useState<AppartsProps[]>([]);
 
     const getData = async () => {
-        const response = await fetch("http://localhost:5001/apparts");
+        const response = await fetch("https://pacific-reaches-55510-1cc818501846.herokuapp.com/apparts");
         const data = await response.json();
         setapparts(data.slice(0, 3));
     };
